7a613fe9b3876370b24a6a48f88fd306824f2f39
13750732 <wtfbro0230@gmail.com>
2025-09-18 17:18:01 +0800

    week02

---
+++ week02/week02-3.cpp
@@ -0,0 +1,20 @@
+///week02-2b.cpp 使用C++2011年新版的字串stoi()功能
+///在CodeBlocks裡，必須開啟 Setting-Compiler...把c++11 打勾
+#include <iostream>///cin cout 讀入資料、印出資料
+#include <string>///string 字串的功能
+using namespace std;///使用「命名空間」標準的std
+int main()
+{
+	string a;///宣告字串a
+	cin >> a;///讀入字串a
+
+	string ans;///宣告字串ans 放答案用
+	int N = a.length();///字串a的長度
+	for(int i=N-1; i>=0; i--){///倒過來的迴圈
+		ans += a[i];///在迴圈哩，把a[i]塞到ans的後面
+	}
+
+	cout << a << '+' << stoi(ans) << '='
+	<< stoi(a) + stoi(ans) << endl;
+}///stoi() is "string to int"把「字串」變成「整數」
+
